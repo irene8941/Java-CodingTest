486edc524d03a0e29b6594775b6a8b28c03aacd8
irene8941 <irene8941@naver.com>
2022-09-15 20:02:01 +0900

    [Add] 너비우선탐색(BFS)

--- BFSSearch.java
+++ BFSSearch.java
@@ -2,11 +2,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-// 기본 선언 HashMap<키타입, 값타입> 변수 = new HashMap<키타입, 값타입>();
-// BFS 알고리즘 구현 => 자료구조 큐(선입선출 방식)를 활용 => 큐의 구현은 ArrayList 클래스를 활용
-// 두 개의 큐 생성 => needVisit 큐, visted 큐
 public class BFSSearch {
     public ArrayList<String> bfsFunctions(HashMap<String, ArrayList<String>> graph, String startNode) {
+        // BFS 알고리즘 구현 => 자료구조 큐(선입선출 방식)를 활용 => 큐의 구현은 ArrayList 클래스를 활용
+        // 두 개의 큐 생성 => needVisit 큐, visted 큐
         ArrayList<String> visited = new ArrayList<>();
         ArrayList<String> needVisit = new ArrayList<>();
 
@@ -25,8 +24,12 @@ public class BFSSearch {
     }
 
     public static void main(String[] args) {
+        // 그래프 표현 방법 : HashMap
+        // HashMap은 '키'와 '값'을 저장하는 자료 구조로, 내부에서 해쉬 함수를 통해, '키' 에 대한 '값' 을 빠르게 검색할 수 있음
+        // 기본 선언 HashMap<키타입, 값타입> 변수 = new HashMap<키타입, 값타입>();
         HashMap<String, ArrayList<String>> graph = new HashMap<String, ArrayList<String>>();
 
+        // HashMap 데이터 추가
         graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
         graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
         graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
