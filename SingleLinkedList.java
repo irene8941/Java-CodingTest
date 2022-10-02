c6c4dec027e0a6e70b6542a20aa1da5358877b7a
irene8941 <irene8941@naver.com>
2022-10-02 21:36:50 +0900

    [Add] 자료구조 (링크드리스트) - 특정 노드 삭제

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -61,21 +61,52 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public boolean delNode(T isData) {
+        if (this.head == null) {
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if (node.data == isData) {    // 삭제할 데이터가 노드 데이터일 경우
+                this.head = this.head.next;
+                return true;
+            } else {
+                while (node.next != null) {
+                    if (node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        // 5개의 노드 생성
         SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
 
         MyLinkedList.addNode(1);
         MyLinkedList.addNode(2);
         MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+
         MyLinkedList.printAll();
 
-        MyLinkedList.addNodeInside(5, 1);
+        // 테스트1: 중간 노드 삭제
+        MyLinkedList.delNode(3);
         MyLinkedList.printAll();
 
-        MyLinkedList.addNodeInside(6, 3);
+        // 테스트2: 맨 앞의 노드(헤드) 삭제
+        MyLinkedList.delNode(1);
         MyLinkedList.printAll();
 
-        MyLinkedList.addNodeInside(7, 20);
+        // 테스트3: 맨 마지막 노드 삭제
+        MyLinkedList.delNode(5);
         MyLinkedList.printAll();
+
+        // 테스트4: 없는 노드 삭제 시도
+        System.out.println(MyLinkedList.delNode(20));
     }
 }
