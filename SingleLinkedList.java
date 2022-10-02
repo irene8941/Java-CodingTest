50bc858d5c55a943b629f22b6733ef6e8fe4595a
irene8941 <irene8941@naver.com>
2022-10-02 21:03:26 +0900

    [Add] 자료구조 (링크드리스트) - 링크드 리스트 데이터 사이에 데이터 추가

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -5,7 +5,7 @@ public class SingleLinkedList<T> {
         T data;
         Node<T> next = null;
 
-        public Node(T data){
+        public Node(T data) {
             this.data = data;
         }
     }
@@ -18,7 +18,6 @@ public class SingleLinkedList<T> {
             while (node.next != null) {
                 node = node.next;
             }
-
             node.next = new Node<T>(data);
         }
     }
@@ -34,12 +33,49 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public Node<T> search(T data) {
+        if (this.head == null) {
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while(node != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.search(isData);
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);    // 원래 Node가 새로운 Node를 가리킴
+            searchedNode.next.next = nextNode;
+        }
+    }
+
     public static void main(String[] args) {
         SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<Integer>();
+
         MyLinkedList.addNode(1);
         MyLinkedList.addNode(2);
         MyLinkedList.addNode(3);
+        MyLinkedList.printAll();
 
+        MyLinkedList.addNodeInside(5, 1);
+        MyLinkedList.printAll();
+
+        MyLinkedList.addNodeInside(6, 3);
+        MyLinkedList.printAll();
+
+        MyLinkedList.addNodeInside(7, 20);
         MyLinkedList.printAll();
     }
 }
